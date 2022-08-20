224c0ca43e5380857d59bec4b8b9f8ffda6df983
Ragnus534 <jakub3389@gmail.com>
2022-08-21 00:06:06 +0200

    First test controller

---
+++ CodingJournal.API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using CodingJournal.API.Data;
+using CodingJournal.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodingJournal.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _dataContext.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _dataContext.Users.FindAsync(id);
+        }
+    }
+}
